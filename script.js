e23f54b05271ca5905ca904f8f48f4d744614fb3
bluez62 <107633296+bluez62@users.noreply.github.com>
2026-03-28 02:52:03 -0500

    Refactor script.js for consistency and formatting
    Added semicolons for consistency and fixed formatting.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const version = "V1.0.0"
-let clicks = 0
-let cps = 0
-let cpc = 1
+const version = "V1.0.0";
+let clicks = 0;
+let cps = 0;
+let cpc = 1;
 
 function onecps() {
   if(clicks >= 50) {
-    clicks -= 50
-    cps += 1
+    clicks -= 50;
+    cps += 1;
   }
+}
+
+function mrLoop() {
+  document.getElementById("ver").innerText = version;
+setTimeout(() {
+  mrLoop();
+}, 1000 / 30);
 }

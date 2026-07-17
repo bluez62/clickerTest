3242bb1568db550da9a8c5a7fba78713c0fe39bd
bluez62 <107633296+bluez62@users.noreply.github.com>
2026-07-16 20:47:53 -0500

    Refactor click cost deduction and display updates
    Refactor click cost handling and update display logic.

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ let cpc1cost = 50;
 
 function onecps() {
   if(clicks >= 50) {
-    clicks -= 50;
+    clicks -= cpc1cost;
     cpc += 1;
-    document.getElementById("counter").innerText = clicks + " Clicks";
     cpc1cost *= 1.15;
     cpc1cost = Math.round(cpc1cost);
-    document.getElementById("cpc1counter").innerText = "+1 CPC - Cost: " + cpc1cost;
+    updateDisplay();
   }
 }
 
@@ -24,6 +23,11 @@ setTimeout(() => {
 function doClick() {
   clicks += cpc
   console.log(clicks)
-  document.getElementById("counter").innerText = clicks + " Clicks"
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById("counter").innerText = clicks + " Clicks";
+  document.getElementById("cpc1counter").innerText = "+1 CPC - Cost: " + cpc1cost;
 }
 mrLoop();
